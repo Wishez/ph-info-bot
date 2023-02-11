2c2a0ae38785e3323af0961961dc2fe8335974d2
Фил Журавлёв <shiningfinger@list.ru>
2023-02-11 11:49:31 +0300

    Add resolvers to graphql schema

--- app/graphql/index.ts
+++ app/graphql/index.ts
@@ -1,10 +1,21 @@
 import typeGQ from 'type-graphql'
 import { ClientResolver } from './Client/Client.resolvers'
+import { ProviderResolver } from './Provider/Provider.resolvers'
+import { ServiceResolver } from './Service/Service.resolvers'
+import { ServiceAttributeResolver } from './ServiceAttribute/ServiceAttribute.resolvers'
+import { ServiceCategoryResolver } from './ServiceCategory/ServiceCategory.resolvers'
 import { UserResolver } from './User/User.resolvers'
 
 const { buildSchema } = typeGQ
 
 export const getSchema = async () =>
   await buildSchema({
-    resolvers: [UserResolver, ClientResolver],
+    resolvers: [
+      UserResolver,
+      ClientResolver,
+      ServiceCategoryResolver,
+      ServiceAttributeResolver,
+      ServiceResolver,
+      ProviderResolver,
+    ],
   })
