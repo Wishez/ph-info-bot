9fa670e06320d2f427ca6a47f1261dfd30a4a6b1
Фил Журавлёв <shiningfinger@list.ru>
2023-02-11 11:48:39 +0300

    Make static client in DataBaseAdapter

--- app/db/DataBaseAdapter.ts
+++ app/db/DataBaseAdapter.ts
@@ -3,11 +3,7 @@ import { Env } from '../config/Env'
 import { EDbStatus } from './types'
 
 export class DataBaseAdapter {
-  client: Redis
-
-  constructor() {
-    this.client = new Redis({ host: 'db', port: 6379, password: Env.dbPassword })
-  }
+  static client = new Redis({ host: 'db', port: 6379, password: Env.dbPassword })
 
   set = <GValue>(key: string, value: GValue): EDbStatus => {
     const resultValue = typeof value === 'object' ? JSON.stringify(value) : value
@@ -16,25 +12,24 @@ export class DataBaseAdapter {
       return EDbStatus.ERROR
     }
 
-    this.client.set(key, resultValue)
+    DataBaseAdapter.client.set(key, resultValue)
 
     return EDbStatus.OK
   }
 
   get = async <GValue>(key: string): Promise<GValue | null> => {
     try {
-      const value = await this.client.get(key).catch(() => null)
-      const result: GValue = value && JSON.parse(value)
+      const value = await DataBaseAdapter.client.get(key).catch(() => null)
 
-      return result
+      return value && JSON.parse(value)
     } catch {
-      const value = await this.client.get(key)
+      const value = await DataBaseAdapter.client.get(key)
 
       return value as GValue
     }
   }
 
   deleteNamespace = (key: string) => {
-    this.client.del(key)
+    DataBaseAdapter.client.del(key)
   }
 }
